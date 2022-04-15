790efe857748896271079c159acaf88e33fe1d72
MiguelDP <migueleken@hotmail.com>
2022-04-15 22:53:49 +0200

    Added tampermonkey script

---
+++ Episode names.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Episode names
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.imdb.com/title/*/episodes?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=imdb.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const outputNames = () => {
+
+        const title = document.querySelector('.aux-content-widget-2.links.subnav a').textContent;
+
+        const $episodes = document.querySelectorAll('.list.detail.eplist>div>div>strong>a');
+
+        const $main = document.querySelector('.subpage_title_block');
+        const $Temp = document.createElement('ul')
+
+
+         Array.prototype.forEach.call($episodes, (episodeName, key) => {
+             const $listItem = document.createElement('li');
+             $listItem.textContent = ((`${title} - S01E${(key+1) >= 10 ? (key+1) : '0'+(key+1)} - ${episodeName.textContent}`).replace(/\/$/, "").replace(':', '').replace('.', '').replace('"', '').replace("'", ''));
+             $Temp.appendChild($listItem)
+            });
+
+        $main.appendChild($Temp)
+
+    };
+
+    outputNames();
+
+
+
+
+
+
+
+
+
+})();
